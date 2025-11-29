938af8c8ac66c7f1d516beee3e3ba55e03293b62
Zakaria Khourbach <zkhourba@e1r10p5.1337.ma>
2025-11-29 20:34:30 +0100

    add rpls 333 332 and remove .d .o

--- srcs/commands/mode.d
+++
@@ -1,5 +0,0 @@
-srcs/commands/mode.o: srcs/commands/mode.cpp \
-  srcs/commands/../../includes/Server.hpp \
-  srcs/commands/../../includes/Command.hpp \
-  srcs/commands/../../includes/Channel.hpp \
-  srcs/commands/../../includes/Client.hpp
