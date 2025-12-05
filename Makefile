c12460abe8dacabd6fe856725b568cd616f7bab7
Soufiane <sofiansofianait@gmail.com>
2025-12-05 16:28:19 +0100

    the bot works fine with nc

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 NAME      = ircserv
+BONUS_NAME  = bot
 CC        = c++
 CFLAGS    =  -Wall -Wextra -Werror -std=c++98 -fsanitize=address
 RM        = rm -rf
@@ -6,9 +7,10 @@ RM        = rm -rf
 SRCS_FILES      = main.cpp Server.cpp Client.cpp AuthCommands.cpp Channel.cpp commands/mode.cpp \
 				  commands/privmsg.cpp commands/join.cpp commands/topic.cpp\
 				  commands/kick.cpp commands/invite.cpp commands/part.cpp
-				  
-SRCS 		= $(addprefix srcs/, $(SRCS_FILES))
+BONUS_FILE = srcs/bot.cpp 
+BONUS_OBJS  = $(BONUS_FILE:.cpp=.o)
 
+SRCS 		= $(addprefix srcs/, $(SRCS_FILES))
 OBJS      = $(SRCS:.cpp=.o)
 
 all: $(NAME)
@@ -16,17 +18,24 @@ all: $(NAME)
 $(NAME): $(OBJS)
 	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
 
+bonus: $(BONUS_NAME)
+
+$(BONUS_NAME): $(BONUS_OBJS)
+	$(CC) $(CFLAGS) $(BONUS_OBJS) -o $(BONUS_NAME)
+
 %.o: %.cpp
 	$(CC) $(CFLAGS) -c $< -o $@ -MMD
 
 clean:
 	$(RM) $(OBJS) $(OBJS:.o=.d)
+	$(RM) $(BONUS_OBJS) 
 
 fclean: clean
 	$(RM) $(NAME)
+	$(RM) $(BONUS_NAME)
 
 re: fclean all
 
 -include $(OBJS:.o=.d)
 
-.PHONY: all clean fclean re
+.PHONY: all clean fclean re bonus
--- srcs/bot.cpp
+++ srcs/bot.cpp
@@ -1,6 +1,4 @@
 #include "../includes/Server.hpp"
-#include <ostream>
-#include <string>
 
 std::vector<std::string> splitVector(const std::string &input_string,
                                                 char delimiter) {
@@ -23,7 +21,7 @@ e_cmd_bot_type getCmdTtype(const std::string & cmdName)
         "!time",
         "!joke",
     };
-    for (size_t i = 0; i < 4; i++) {
+    for (size_t i = 0; i < 3; i++) {
         if (cmdName == botCmdsNames[i])
             return e_cmd_bot_type(i);
     }
@@ -31,58 +29,53 @@ e_cmd_bot_type getCmdTtype(const std::string & cmdName)
 }
 
 void SendPrivateMessage(int bot_fd, std::string & SendClient, const std::string &Message){
-    std::string FullMessage =  "PRIVMSG " + SendClient + " :" + Message + "\r\n" ;
-    std::cout << "FullMessage: " << FullMessage << std::endl;
-    if (send(bot_fd, Message.c_str(), Message.length(), 0) == -1){
+    std::string FullMessage = "PRIVMSG " + SendClient + " :" + Message + "\r\n";
+    std::cout << FullMessage << std::endl;
+    if (send(bot_fd, FullMessage.c_str(), FullMessage.length(), 0) == -1){
         throw std::runtime_error("Bot connection failed during send.");
     }
 }
 
 
-// void handleBotJoke(int bot_fd, std::string & SendClient, const std::vector<std::string>& args) {
-//     
-//     if (args.size() != 1) { 
-//         // Queue error: "!joke takes no arguments. Usage: !joke"
-//         return; 
-//     }
-//     std::vector<std::string> jokes;
-//     jokes.push_back("I’d tell them a UDP joke but there’s no guarantee that they would get it.");
-//     jokes.push_back("Q. How do robots eat pizza? A. One byte at a time.");
-//     jokes.push_back("Q. How did the first program die? A. It was executed.");
-//     jokes.push_back("Q. Why do Java programmers wear glasses? A. They can’t C#");
-//     jokes.push_back("Q. What is the difference between C++ and C? A. Just 1.");
-//     jokes.push_back("Q. Whats the object-oriented way to become wealthy? A. Inheritance");
-//     jokes.push_back("Q. What do computers and air conditioners have in common? A. They both become useless when you open windows.");
-//     size_t joke_count = jokes.size();
-//     int random_index = rand() % joke_count; 
-//     std::string selected_joke = jokes[random_index];
-//     SendPrivateMessage(bot_fd, SendClient, selected_joke);
-// }
-//
-// void HelpCmd(int bot_fd, std::string & Sendclient, std::vector<std::string>& arguments){
-//     std::string message;
-//     std::vector<std::string> HelpMessages;
-//     HelpMessages.push_back("-- FTBot Command List --");
-//     HelpMessages.push_back("help :Displays this command list.");
-//     HelpMessages.push_back("time :Displays the current server time and date.");
-//     HelpMessages.push_back("------------------------");
-//     std::cout << "here" << std::endl;
-//     for (size_t i = 0; i < HelpMessages.size(); i++)
-//          SendPrivateMessage(bot_fd, Sendclient, HelpMessages[i]);
-// }
-//
-// void timeCmd(int bot_fd, std::string & Sendclient, std::vector<std::string>& arguments){
-//      std::string message;
-//      time_t RawSeconds;
-//      time(&RawSeconds);
-//      struct tm *TimeInfo = localtime(&RawSeconds);
-//      char TimeBuffer[100];
-//      size_t len = strftime(TimeBuffer, sizeof(TimeBuffer), 
-//                            "Current server time: %H:%M:%S on %Y-%m-%d", 
-//                            TimeInfo);
-//      if (len > 0)
-//          SendPrivateMessage(bot_fd, Sendclient, TimeBuffer);
-// }
+ void handleBotJoke(int bot_fd, std::string & SendClient) {
+     std::vector<std::string> jokes;
+     jokes.push_back("I’d tell them a UDP joke but there’s no guarantee that they would get it.");
+     jokes.push_back("Q. How do robots eat pizza? A. One byte at a time.");
+     jokes.push_back("Q. How did the first program die? A. It was executed.");
+     jokes.push_back("Q. Why do Java programmers wear glasses? A. They can’t C#");
+     jokes.push_back("Q. What is the difference between C++ and C? A. Just 1.");
+     jokes.push_back("Q. Whats the object-oriented way to become wealthy? A. Inheritance");
+     jokes.push_back("Q. What do computers and air conditioners have in common? A. They both become useless when you open windows.");
+     size_t joke_count = jokes.size();
+     int random_index = rand() % joke_count; 
+     std::string selected_joke = jokes[random_index];
+     SendPrivateMessage(bot_fd, SendClient, selected_joke);
+ }
+
+void HelpCmd(int bot_fd, std::string & Sendclient){
+    std::string message;
+    std::vector<std::string> HelpMessages;
+    HelpMessages.push_back("-- FTBot Command List --");
+    HelpMessages.push_back("!help :Displays this command list.");
+    HelpMessages.push_back("!time :Displays the current server time and date.");
+    HelpMessages.push_back("!joke :Tells a random programming joke.");
+    HelpMessages.push_back("------------------------");
+    for (size_t i = 0; i < HelpMessages.size(); i++)
+         SendPrivateMessage(bot_fd, Sendclient, HelpMessages[i]);
+}
+
+void timeCmd(int bot_fd, std::string & Sendclient){
+     std::string message;
+     time_t RawSeconds;
+     time(&RawSeconds);
+     struct tm *TimeInfo = localtime(&RawSeconds);
+     char TimeBuffer[100];
+     size_t len = strftime(TimeBuffer, sizeof(TimeBuffer), 
+                           "Current server time: %H:%M:%S on %Y-%m-%d", 
+                           TimeInfo);
+     if (len > 0)
+         SendPrivateMessage(bot_fd, Sendclient, TimeBuffer);
+}
 std::string extractNick(const std::string& prefix) {
     size_t exclamationPos = prefix.find('!');
     if (exclamationPos != std::string::npos) {
@@ -92,6 +85,15 @@ std::string extractNick(const std::string& prefix) {
         return prefix.substr(1);
         
     return prefix;
+}
+std::string getCmd(std::string msg) {
+    // 1. Safety Check: Empty string
+    if (msg.empty()) return "";
+
+    if (msg[0] == ':') {
+        msg = msg.substr(1);
+    }
+    return msg;
 }
 void processBotCommand(int bot_fd, std::string & FullMessage){
     std::vector<std::string> splitedCommand =
@@ -114,29 +116,27 @@ void processBotCommand(int bot_fd, std::string & FullMessage){
         return;
     }
     std::string prefix = splitedCommand[0];    
-    std::string command = splitedCommand[1];   
-    std::string target = splitedCommand[2];    
     std::string msg = splitedCommand[3];
     std::string Sendclient = extractNick(prefix);
+    std::string cmd = getCmd(msg);
     std::cout << Sendclient << std::endl;
-    return;
-    // e_cmd_bot_type cmdType = getCmdTtype(splitedCommand[1]) ;
-    // switch (cmdType) {
-    //     case BOT_CMD_HELP:
-    //         HelpCmd(bot_fd, Sendclient, splitedCommand );
-    //         break;
-    //     case BOT_CMD_TIME:
-    //         timeCmd(bot_fd, Sendclient, splitedCommand );
-    //         break;
-    //     case BOT_CMD_JOKE:
-    //         handleBotJoke(bot_fd, Sendclient, splitedCommand );
-    //         break;
-    //     case BOT_CMD_UNKNOWN:
-    //         std::string message = splitedCommand[1] + "is not a recognized command. Use help for a list of available commands.";
-    //         SendPrivateMessage(bot_fd, Sendclient, message);
-    //         break;
-    //     // ...
-    // }
+    std::cout << cmd << std::endl;
+    e_cmd_bot_type cmdType = getCmdTtype(cmd) ;
+    switch (cmdType) {
+        case BOT_CMD_HELP:
+            HelpCmd(bot_fd, Sendclient);
+            break;
+        case BOT_CMD_TIME:
+            timeCmd(bot_fd, Sendclient);
+            break;
+        case BOT_CMD_JOKE:
+            handleBotJoke(bot_fd, Sendclient);
+            break;
+        case BOT_CMD_UNKNOWN:
+            std::string message = splitedCommand[1] + "is not a recognized command. Use help for a list of available commands.";
+            // SendPrivateMessage(bot_fd, Sendclient, message);
+            break;
+    }
 }
 void SendMessage(int bot_fd, const std::string &Message){
     if (send(bot_fd, Message.c_str(), Message.length(), 0) == -1){
@@ -162,10 +162,13 @@ std::string getServerAuth(int bot_socket_fd){
         if (bytes_read <= 0) {
             if (bytes_read == 0)
                 throw std::runtime_error("Server closed connection unexpectedly.");
-            else
-                throw std::runtime_error("Recv error during auth.");
+            else if (bytes_read < 0) {
+                if (errno != EWOULDBLOCK || errno != EAGAIN)
+                    std::cerr << "recv error: " << strerror(errno) << std::endl;
+            }
         }
-        bot_in_buffer.append(buff, bytes_read);
+        if (bytes_read > 0)
+            bot_in_buffer.append(buff, bytes_read);
     }
     int dilimiterLen;
     size_t pos_found = bot_in_buffer.find("\r\n");
@@ -196,11 +199,11 @@ int setup_connection(const std::string& server_ip, int port, const std::string& 
         close(bot_socket_fd);
         throw std::runtime_error("Connection refused or failed.");
     }
-    // if (fcntl(bot_socket_fd, F_SETFL, O_NONBLOCK) == -1)
-    // {
-    //     close(bot_socket_fd); 
-    //     throw std::runtime_error("fcntl failed.");
-    // }
+    if (fcntl(bot_socket_fd, F_SETFL, O_NONBLOCK) == -1)
+    {
+        close(bot_socket_fd); 
+        throw std::runtime_error("fcntl failed.");
+    }
     std::string bot_user = "BotUser";
     SendMessage(bot_socket_fd, "PASS " + password + "\r\n");
     SendMessage(bot_socket_fd, "NICK " + nick_name + "\r\n");
@@ -241,7 +244,7 @@ void ProcessAndExtractCommands(int bot_fd, std::string &_ReadBuffer) {
   }
 }
 
-void start_bot_loop(int bot_fd) {
+void start_bot_loop(int bot_fd){
     
     std::string bot_in_buffer;
     while (true) {
@@ -252,10 +255,15 @@ void start_bot_loop(int bot_fd) {
 	    if (bytes_read == 0){
 	        close(bot_fd);
 	        return;
-	    }else {
+	    }else if (bytes_read > 0)
+        {
             buff[bytes_read] = '\0';
             bot_in_buffer.append(buff, bytes_read);
             ProcessAndExtractCommands(bot_fd, bot_in_buffer);
+
+        }else if (bytes_read < 0) {
+            if (errno != EWOULDBLOCK || errno != EAGAIN)
+                std::cerr << "recv error: " << strerror(errno) << std::endl;
         }
     }
 }
@@ -288,6 +296,5 @@ int main(int argc, char *argv[]) {
             close(bot_fd);
         return 1;
     }
-    
     return 0;
 }
